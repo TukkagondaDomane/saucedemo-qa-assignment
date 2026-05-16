b0cb3c6c25eac1e410f4d33bed2f4bd8dc267d9e
Tukkagonda Domane <142110823+TukkagondaDomane@users.noreply.github.com>
2026-05-16 12:21:48 +0530

    Add LoginPage class for authentication automation

---
+++ playwright-automation/pages/LoginPage.ts
@@ -0,0 +1,22 @@
+import { Page, expect } from "@playwright/test";
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async gotoLoginPage() {
+    // await this.page.goto("https://www.saucedemo.com/");
+    await this.page.goto("/");
+  }
+
+  async login(username: string, password: string) {
+    await this.page.locator('[data-test="username"]').fill(username);
+
+    await this.page.locator('[data-test="password"]').fill(password);
+
+    await this.page.locator('[data-test="login-button"]').click();
+  }
+
+  async verifySuccessfulLogin() {
+    await expect(this.page).toHaveURL(/inventory/);
+  }
+}
